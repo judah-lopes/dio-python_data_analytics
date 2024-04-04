63900ffbadf1c1526be90470b0fb4b50a9bc29a7
JudahzinOhBrabo <lopes.judah@gmail.com>
2024-04-04 13:54:38 -0300

    doc: Tipos de Dados
    Commit com:
    - tópico "Tipos de Dados"

---
+++ Mód 2 - Introdução à Ciência de Dados com Python/Aula 2 - Conhecendo a Linguagem de Programação Python/script.py
@@ -0,0 +1,27 @@
+#! -------- Tópico 1 - Tipos de dados --------
+
+# ===== etapa 1 - tipos =====
+#
+#   Texto     = str
+#   Numérico  = int, float, complex
+#   Sequência = list, tuple, range
+#   Mapa      = dict
+#   Coleção   = set, frozenset
+#   Booleano  = bool
+#   Binário   = bytes, bytearray, memoryview
+
+# ===== etapa 2 - tipos numericos =====
+#
+#   int()   = inteiro, negativo ou positivo
+#   float() = com "virgula"
+
+# ===== etapa 3 - booleano e strings =====
+#
+#   bool() = true ou false (subclasse de int, 0 = false, 1+ = true)
+#   str()  = cadeia de caracteres
+
+print(1 + 3 + 2)
+print(1.5 + 4 + 3.5)
+print(True)
+print(False)
+print('carlito teves')
