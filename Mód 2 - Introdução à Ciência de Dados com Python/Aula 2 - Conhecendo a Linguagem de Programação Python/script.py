a7e4099a88af2c88b4ae23b097f6162bbc5f3c29
JudahzinOhBrabo <lopes.judah@gmail.com>
2024-04-04 18:26:07 -0300

    doc: Variáveis e Constantes
    Commit com:
    - tópico "Variáveis e Constantes"

--- Mód 2 - Introdução à Ciência de Dados com Python/Aula 2 - Conhecendo a Linguagem de Programação Python/script.py
+++ Mód 2 - Introdução à Ciência de Dados com Python/Aula 2 - Conhecendo a Linguagem de Programação Python/script.py
@@ -1,22 +1,25 @@
-#! -------- Tópico 2 - Modo Interativo --------
+#! -------- Tópico 3 - Variáveies e Constantes --------
 
-# ===== etapa 1 - Usando o Modo Interativo =====
-#
-#   O modo interativo possibilita o dev escrever o código
-#  e ver o resultado na mesma hora. (tipo Live Preview).
-#   Para executar o modo, tem duas maneiras:
-#       - Escrever "python" no terminal, chamando o executor
-#       - Usando a flag -i no arquivo "python - i scrip.py"
-
-# ===== etapa 2 - Funções dir e help =====
-#   _DIR_____________
-#   dir()    = lista do escopo local
-#   dis(arg) = lista de atributos válidos para o objeto
+# ===== etapa 1 - constante =====
 #   
-#   _HELP____________
-#   help() = invoca o sistema de ajuda, tipo o google
-#   help(arg) =  invoca todas as info do argumento, inclusive os metodos
-#
-#
-#
-print('pedro')
+#   Não existe constante em python. Como >CONVENÇÃO<, quando
+#   queremos uma constante, escrevemos o nome da variável TODA em maiúsculo.
+
+AGE = 19
+
+print(AGE)
+
+# ===== etapa 2 - boas práticas =====
+#   
+#   - O padrão dos nomes deve ser o snake case. ("preço_total")
+#   - Escolher nomes sujestivos.
+#   - Nome de constantes todos em MAIÚSCULO.
+
+SALDO = 10000
+
+limite_saque_diário = 1000
+
+dias_para_acabar_o_saldo = SALDO / limite_saque_diário
+
+print(f'Serão necessários {int(dias_para_acabar_o_saldo)} para sacando o limite de R${float(limite_saque_diário)} para o dinheiro do saldo acabar.')
+
