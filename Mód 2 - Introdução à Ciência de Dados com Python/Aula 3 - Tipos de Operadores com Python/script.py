2774b730fa4a8391bcf62107ada595e75069a0a6
JudahzinOhBrabo <lopes.judah@gmail.com>
2024-04-14 16:12:16 -0300

    doc: Operadores Aritméticos
    Commit com:
    -Tópico "Operadores Aritméticos"

---
+++ Mód 2 - Introdução à Ciência de Dados com Python/Aula 3 - Tipos de Operadores com Python/script.py
@@ -0,0 +1,32 @@
+# -------- Tópico 1 - Operadores Aritméticos --------
+
+#! ===== etapa 1 - Conhecendo os operadores aritméticos =====
+#   
+#?  ADIÇÃO
+print(4 + 4)
+#________________________
+#?  SUBTRAÇÃO 
+print(4 - 4)
+#________________________
+#?  MUTIPLICAÇÃO
+print(4 * 4)
+#________________________
+#?  DIVISÃO
+print(10 / 4)
+#________________________
+#?  DIVISÃO sem resto
+print(10 // 4)
+#________________________
+#?  MÓDULO
+print(10 % 4)
+#________________________
+#?  EXPONÊNCIAÇÃO
+print(10 ** 4)
+#   
+#! ===== etapa 2 - Precedência de operadores =====
+#   
+#   A ordem das operações é 
+#   - parenteses
+#   - expoentes
+#   - mutiplicações e divisões
+#   - adição e subtração
