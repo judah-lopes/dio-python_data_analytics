970bf0ce6820e3d2946ddb27e3a2cef6e1ad0136
JudahzinOhBrabo <lopes.judah@gmail.com>
2024-04-15 18:02:12 -0300

    doc: Estruturas de Repetição
    Commit com:
    - tópico "Estruturas de Repetição"

--- Mód 2 - Introdução à Ciência de Dados com Python/Aula 4 - Estruturas Condicionais e de Repetição em Python/script.py
+++ Mód 2 - Introdução à Ciência de Dados com Python/Aula 4 - Estruturas Condicionais e de Repetição em Python/script.py
@@ -1,46 +1,51 @@
-# # === etapa 1 - if / if-else / elif ===
+# ----- for -----
 
-# # ---- if ----
-saldo = 2000.0
-saque = float(input('Qual o valor que desejas sacar?'))
+texto = input('Informe um texto: ')
+VOGAIS = 'AEIOU'
 
-if saldo >= saque:
-	print('Saque realizado com sucesso.')
-	
-if saldo < saque:
-	print('Não foi possível realizar o saque.')
-	
-# # ---- if-else ----
-saldo = 2000.0
-saque = float(input('Qual o valor que desejas sacar?'))
+for letra in texto:
+    if letra.upper() in VOGAIS:
+        print(letra, end='')
+print()
 
-if saldo >= saque:
-    saldo -= saque
-    print('Saque realizado com sucesso.')
+# ----- range() -----
+list(range(4))
+
+# ----- range com for -----
+for numero in range(11):
+	print(numero, end=" ")
+    
+print()
+
+for numero in range(0, 51, 5):
+	print(numero, end=" ")
+      
+# ----- while -----
+print()
+
+opcao = -1
+
+while opcao != 0:
+    opcao = int(input('[1] Sacar \n[2] Extrato \n[0] Sair\n'))
+
+    if opcao == 1:
+          print('sacando...')
+    elif opcao == 2:
+          print('exibindo extrato...')
+
+# ----- while/else -----
+print()
+
+opcao = -1
+
+while opcao != 0:
+    opcao = int(input('[1] Sacar \n[2] Extrato \n[0] Sair\n'))
+
+    if opcao == 1:
+          print('sacando...')
+    elif opcao == 2:
+          print('exibindo extrato...')
 else:
-    print('Não foi possível realizar o saque.')
-
-# ---- elif ----
-opcao = int(input('Informe uma opção: \n[1] Sacar \n[2] Estrato\n'))
-
-if opcao == 1:
-	valor = float(input('Qual a quantia a ser sacada?'))
-elif opcao == 2:
-	print('Exibindo o extrato...')
-else:
-	exit('Opção inválida')
-
-# === etapa 2 - if aninhado ===
-if conta_normal:
-    if saldo >= saque:
-        print('Saque realizado com sucesso')
-    elif saque <= (saldo + cheque_especial):
-        print('Saque realizado com sucesso.') 
-elif conta_universitária:
-    if saldo >= saque:
-        print('Saque realizado com sucesso')
-    else:
-    	print('Saldo insuficiente')
-
-# === etapa 3 - if ternário ===
-status = 'Sucesso' if saldo >= saque else 'Falha'
+	print('Obrigado, até logo!')
+      
+    
