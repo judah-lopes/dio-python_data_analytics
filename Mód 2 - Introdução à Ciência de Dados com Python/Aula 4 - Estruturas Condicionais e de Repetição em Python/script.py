a066fbc1bda50d86f59a5806d3a555974413c1f2
JudahzinOhBrabo <lopes.judah@gmail.com>
2024-04-15 16:32:29 -0300

    doc: Estruturas Condicionais
    Commit com:
    - tópico "Estruturas Condicionais"

--- Mód 2 - Introdução à Ciência de Dados com Python/Aula 4 - Estruturas Condicionais e de Repetição em Python/script.py
+++ Mód 2 - Introdução à Ciência de Dados com Python/Aula 4 - Estruturas Condicionais e de Repetição em Python/script.py
@@ -1,18 +1,46 @@
-def sacar(valor):
-    saldo = 300
-    
-    if valor <= saldo:
-        saldo -= valor
-        print(f'Saque sucedido. Seu saldo agora é R${float(saldo)}')
-        
+# # === etapa 1 - if / if-else / elif ===
 
-sacar(200)
+# # ---- if ----
+saldo = 2000.0
+saque = float(input('Qual o valor que desejas sacar?'))
 
-def depositar(valor):
-    saldo = 1000
+if saldo >= saque:
+	print('Saque realizado com sucesso.')
+	
+if saldo < saque:
+	print('Não foi possível realizar o saque.')
+	
+# # ---- if-else ----
+saldo = 2000.0
+saque = float(input('Qual o valor que desejas sacar?'))
 
-    if valor > 0:
-        saldo += valor
-        print(f'Parabéns! Depósito efetuado com sucesso! Seu saldo agora é R${float(saldo)}')
+if saldo >= saque:
+    saldo -= saque
+    print('Saque realizado com sucesso.')
+else:
+    print('Não foi possível realizar o saque.')
 
-depositar(200)
+# ---- elif ----
+opcao = int(input('Informe uma opção: \n[1] Sacar \n[2] Estrato\n'))
+
+if opcao == 1:
+	valor = float(input('Qual a quantia a ser sacada?'))
+elif opcao == 2:
+	print('Exibindo o extrato...')
+else:
+	exit('Opção inválida')
+
+# === etapa 2 - if aninhado ===
+if conta_normal:
+    if saldo >= saque:
+        print('Saque realizado com sucesso')
+    elif saque <= (saldo + cheque_especial):
+        print('Saque realizado com sucesso.') 
+elif conta_universitária:
+    if saldo >= saque:
+        print('Saque realizado com sucesso')
+    else:
+    	print('Saldo insuficiente')
+
+# === etapa 3 - if ternário ===
+status = 'Sucesso' if saldo >= saque else 'Falha'
