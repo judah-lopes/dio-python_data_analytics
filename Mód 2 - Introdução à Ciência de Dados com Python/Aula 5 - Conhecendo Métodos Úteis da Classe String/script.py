a3fcf41023cfe4b9f7b832738c30e035f6c3e72d
JudahzinOhBrabo <lopes.judah@gmail.com>
2024-04-17 15:01:13 -0300

    doc: String de Linhas Múltiplas
    Commit com:
    - tópico "String de Linhas Múltiplas"

--- Mód 2 - Introdução à Ciência de Dados com Python/Aula 5 - Conhecendo Métodos Úteis da Classe String/script.py
+++ Mód 2 - Introdução à Ciência de Dados com Python/Aula 5 - Conhecendo Métodos Úteis da Classe String/script.py
@@ -1,33 +1,12 @@
-# ====== etapa 1 - fatiamento de strings ======
+# ====== etapa 1 - string de linhas múltiplas ======
 
-# > EXEMPLO
+nome = 'Pedro'
 
-nome = 'Pedro Judah Gomes Nogueira Lopes'
+mensagem = f"""
+Olá, meu nome é {nome}!
+    Eu estou apredendo Python.
+       Isso é muito massa
+"""
 
-# ----- para um caractere específico -----
+print(mensagem)
 
-print(nome[0])
-
-# ----- para um grupo de caracteres *a partir da posição inicial* (:...) -----
-
-print(nome[:5])      # <- do início : até posição 5 - 1 
-
-# ----- para um todos os caracteres *a partir de um início* (...:) -----
-
-print(nome[6:])      # <- da posição 6 : pra frente
-
-# ----- para um grupo de caracteres especificado (...:...)  -----
-
-print(nome[6:11])   # <- da posição 6 : até a posição 11 - 1
-
-# ----- para um grupo de caracteres especificado e com um step (...:...:...) -----
-
-print(nome[6:11:2]) # <- da posição 6 : até a 11 - 1 : pulando de 2 em 2 caracteres
-
-# ----- para a string sem nenhum parâmetro (:) -----
-
-print(nome[:])
-
-# ----- para a string invertida -----
-
-print(nome[::-1])
